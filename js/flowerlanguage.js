a8f512204d5257f312cdbbe492bb7e3b776fc38e
illumyST <illumy.design@gmail.com>
2023-03-19 23:20:41 +0800

    0319

--- js/flowerlanguage.js
+++ js/flowerlanguage.js
@@ -1,5 +1,9 @@
 let bar = document.getElementsByClassName("bar")[0];
+
 let aside = document.getElementsByClassName("aside")[0]
-bar.addEventListener("click",function(){
-    aside.classList.add("-on");
+
+bar.addEventListener("click", function () {
+    aside.classList.toggle("-on");
+    bar.children[0].classList.toggle("-on");
+    bar.children[1].classList.toggle("-on");
 });
