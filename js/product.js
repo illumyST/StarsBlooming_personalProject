a8a0345dff888750ac95467f05484bf01371a797
illumyST <illumy.design@gmail.com>
2023-04-04 14:41:33 +0800

    product RWD

---
+++ js/product.js
@@ -0,0 +1,7 @@
+let menuOl = document.getElementsByClassName("menuOl")[0];
+
+menuOl.addEventListener('click',function(e){
+    e.preventDefault();
+    this.classList.toggle("-on");
+    this.querySelector("ol").classList.toggle("-on");
+})
